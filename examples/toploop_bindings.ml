5f23831f3a50d65571c237cb1585bc9bcb72d385
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2020-10-31 10:29:07 -0400

    Initial patch

--- examples/toploop_bindings.ml
+++ examples/toploop_bindings.ml
@@ -102,12 +102,15 @@ let toploop_eval str ~verbose =
   | exn -> raise (Py.Err (SyntaxError, exn_to_string exn ~code:str))
 ;;
 
+let dummy_loc =
+  Warnings.({loc_start=Lexing.dummy_pos; loc_end=Lexing.dummy_pos; loc_ghost=false})
+
 let toploop_eval_and_get typerep str =
   let eval_value (type a) (typerep : a Typerep.t) =
     toploop_eval
       ~verbose:false
       (Printf.sprintf "let out : %s = (%s);;" (Py_typerep.to_ocaml typerep) str);
-    let path, _ = Env.lookup_value (Lident "out") !Toploop.toplevel_env in
+    let path, _ = Env.lookup_value ~loc:dummy_loc (Lident "out") !Toploop.toplevel_env in
     let obj = Toploop.eval_value_path !Toploop.toplevel_env path in
     Py_typerep.ocaml_to_python typerep (Caml.Obj.obj obj)
   in
@@ -117,7 +120,8 @@ let toploop_eval_and_get typerep str =
 
 let toploop_eval_and_get_no_type str =
   toploop_eval ~verbose:false (Printf.sprintf "let out = (%s);;" str);
-  let path, value_description = Env.lookup_value (Lident "out") !Toploop.toplevel_env in
+  let path, value_description =
+    Env.lookup_value ~loc:dummy_loc (Lident "out") !Toploop.toplevel_env in
   let obj = Toploop.eval_value_path !Toploop.toplevel_env path in
   let (T typerep) =
     Type.of_type_desc value_description.val_type.desc ~env:(Module_env.create ())
